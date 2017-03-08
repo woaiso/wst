a1f52821fbed6fcd41a9ca32a37cfc2b2758e459
yunful <yunful@jumei.com>
2017-03-08 18:19:33 +0800

    add router

--- src/client/dashboard/dashboard.tsx
+++ src/client/dashboard/dashboard.tsx
@@ -4,10 +4,15 @@ import { Layout, Menu, Icon } from 'antd';
 const { Header, Sider, Content, Footer } = Layout;
 import './dashboard.css';
 import DataQuery from './dataQuery';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 
 export default class DashBoard extends React.Component<any, any> {
 	state = {
-		collapsed: true,
+		collapsed: false,
 	};
 	toggle = () => {
 		this.setState({
@@ -16,44 +21,42 @@ export default class DashBoard extends React.Component<any, any> {
 	}
 	render() {
 		return (
-			<div className="dashboard">
-				<Layout>
-					<Sider
-						trigger={null}
-						collapsible
-						collapsed={this.state.collapsed}
-					>
-						<div className="logo" />
-						<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-							<Menu.Item key="1">
-								<Icon type="user" />
-								<span className="nav-text">nav 1</span>
-							</Menu.Item>
-							<Menu.Item key="2">
-								<Icon type="video-camera" />
-								<span className="nav-text">nav 2</span>
-							</Menu.Item>
-							<Menu.Item key="3">
-								<Icon type="upload" />
-								<span className="nav-text">nav 3</span>
-							</Menu.Item>
-						</Menu>
-					</Sider>
-					<Layout style={{ background: '#FFF' }}>
-						<Header style={{ background: '#333', padding: 0 }}>
-							<Icon
-								className="trigger"
-								style={{ color: '#FFF' }}
-								type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-								onClick={this.toggle}
-							/>
-						</Header>
-						<Content className="content-wrap">
-							<DataQuery />
-						</Content>
+			<Router>
+				<div className="dashboard">
+					<Layout>
+						<Sider
+							trigger={null}
+							collapsible
+							collapsed={this.state.collapsed}
+						>
+							<div className="logo" />
+							<Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+								<Menu.Item key="1">
+									<Icon type="hdd" />
+									<span className="nav-text">文章数据</span>
+								</Menu.Item>
+								<Menu.Item key="2">
+									<Icon type="code-o" />
+									<span className="nav-text">规则管理</span>
+								</Menu.Item>
+							</Menu>
+						</Sider>
+						<Layout style={{ background: '#FFF' }}>
+							<Header style={{ background: '#333', padding: 0 }}>
+								<Icon
+									className="trigger"
+									style={{ color: '#FFF' }}
+									type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+									onClick={this.toggle}
+								/>
+							</Header>
+							<Content className="content-wrap">
+								<DataQuery />
+							</Content>
+						</Layout>
 					</Layout>
-				</Layout>
-			</div>
+				</div>
+			</Router>
 		);
 	}
 }
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -4,14 +4,26 @@ import { Button } from 'antd';
 import './index.css';
 // import LoginPage from './pages/login';
 import DashBoard from './dashboard';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
 class Bootstrap extends React.Component<any, any> {
 	render() {
 		return (
-			<div>
-				<DashBoard />
-			</div>
+			<Router>
+				<div>
+					<Route exact path="/" component={Home} />
+					<Route path="/dashboard" component={DashBoard} />
+				</div>
+			</Router>
 		);
 	}
 }
+
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+		<Link to="/dashboard"><Button>DashBoard</Button></Link>
+	</div>
+)
 
 ReactDOM.render(<Bootstrap />, document.getElementById('root'));
