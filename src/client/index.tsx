72c5dd2d8ed14cac49941912f36045248b5f5f3e
woaiso <woaiso@woaiso.com>
2017-03-26 22:04:31 +0800

    redesign main page

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Button } from 'antd';
+import { Button, Row, Col } from 'antd';
 import './index.css';
-// import LoginPage from './pages/login';
+import LoginPage from './pages/login';
 import DashBoard from './dashboard';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
@@ -20,9 +20,23 @@ class Bootstrap extends React.Component<any, any> {
 }
 
 const Home = () => (
-	<div>
-		<h2>Home</h2>
-		<Link to="/dashboard"><Button>DashBoard</Button></Link>
+	<div className="mainBoard">
+		{/*<LoginPage />*/}
+		<header id="header" className="clearfix">
+			<nav className="global-nav">
+				<Row>
+					<Col xs={2} sm={4} md={6} lg={8} xl={10}>Logo</Col>
+					<Col xs={20} sm={16} md={12} lg={8} xl={4}>Col</Col>
+					<Col xs={2} sm={4} md={6} lg={8} xl={10}><Link to="/dashboard"><Button type="dashed" ghost>DashBoard</Button></Link></Col>
+				</Row>
+			</nav>
+		</header>
+		<section>
+			<div className="slide" />
+			<div className="slide" />
+			<div className="slide" />
+			<div className="slide" />
+		</section>
 	</div>
 )
 
