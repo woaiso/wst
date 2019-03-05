004fec2c4a5a53077cc4c92c39450dbdb65272c9
woaiso <woaiso@woaiso.com>
2019-03-05 10:08:45 +0800

    update

--- src/server/services/__spec__/vides.spec.ts
+++ src/server/services/__spec__/vides.spec.ts
@@ -4,14 +4,13 @@
 import Video from './../video';
 import { fetchWithProxy } from './../fetch';
 describe('fetch video and extract', () => {
-	const video = new Video();
-	const url = '';
+  const video = new Video();
+  const url = '';
 
-	it('extract Article has text', () => {
-		fetchWithProxy(url)
-			.then((html) => {
-				const postMessage = video.extractArticle(html, { url: url });
-				expect(postMessage.id).toBe(3201231);
-			});
-	});
+  it('extract Article has text', () => {
+    fetchWithProxy(url).then((html) => {
+      const postMessage = video.extractArticle(html, { url: url });
+      expect(postMessage.id).toBe(3201231);
+    });
+  });
 });
